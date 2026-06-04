78b6e86df81083d8899998b0936535caa780396f
DESKTOP-4GPUFAE\Lenovo <horaoen@gmail.com>
2026-06-04 17:42:47 +0800

    Revert "update"
    This reverts commit 406d298871ba1d40b5b4110c6a138b653307f0ca.

--- vite.config.ts
+++ vite.config.ts
@@ -7,17 +7,6 @@ export default defineConfig({
   plugins: [
     vue(),
     tailwindcss(),
-    {
-      name: 'fix-ts-mime',
-      configureServer(server) {
-        server.middlewares.use((req, res, next) => {
-          if (req.url?.match(/\.(ts|tsx|mts|cts)$/)) {
-            res.setHeader('Content-Type', 'application/javascript')
-          }
-          next()
-        })
-      },
-    },
   ],
   resolve: {
     alias: {
