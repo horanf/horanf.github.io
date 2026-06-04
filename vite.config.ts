406d298871ba1d40b5b4110c6a138b653307f0ca
DESKTOP-4GPUFAE\Lenovo <horaoen@gmail.com>
2026-06-04 17:35:26 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,17 @@ export default defineConfig({
   plugins: [
     vue(),
     tailwindcss(),
+    {
+      name: 'fix-ts-mime',
+      configureServer(server) {
+        server.middlewares.use((req, res, next) => {
+          if (req.url?.match(/\.(ts|tsx|mts|cts)$/)) {
+            res.setHeader('Content-Type', 'application/javascript')
+          }
+          next()
+        })
+      },
+    },
   ],
   resolve: {
     alias: {
